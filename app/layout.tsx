9afc231103febef882636c5398a0e72218a27914
Rajan1807-dev <rajan.nitjam@gmail.com>
2024-09-18 10:38:43 +0530

    chore: ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} wrapper`}>
         {children}
       </body>
     </html>
